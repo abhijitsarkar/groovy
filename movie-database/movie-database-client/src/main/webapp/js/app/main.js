8d277413dec8b8de91159f48eef4d4c09add8332
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2014-04-09 18:41:45 -0400

    Handled form submission on enter.

--- movie-database/movie-database-client/src/main/webapp/js/app/main.js
+++ movie-database/movie-database-client/src/main/webapp/js/app/main.js
@@ -2,8 +2,8 @@ require(["jquery", "dataTables"], function(jquery, dataTables) {
 
     var setUpDefaults = function() {
         jquery("#get").prop("checked", true);
-        jquery("#url").attr("placeholder", "URL");
-        jquery("#go").val("Search");
+        jquery(".url").attr("placeholder", "URL");
+        jquery(".go").val("Search");
     };
 
     setUpDefaults();
@@ -12,15 +12,8 @@ require(["jquery", "dataTables"], function(jquery, dataTables) {
          var getRequest = jquery("#get").is(":checked");
          var goVal = getRequest ? "Search" : "Index";
 
-         jquery("#go").val(goVal);
+         jquery(".go").val(goVal);
     });
-
-    // This preserves the sort order.
-    var repopulateTable = function(table, data) {
-        jquery(table).dataTable().fnClearTable(false);
-        jquery(table).dataTable().fnAddData(data);
-        jquery(table).dataTable().fnDraw();
-    };
 
     var createNewTable = function(data) {
         var formatGenres = function(genres, type, full) {
@@ -49,41 +42,36 @@ require(["jquery", "dataTables"], function(jquery, dataTables) {
                     "aTargets": [2],
                     "mRender": formatReleaseDate
                 }
-            ]
+            ],
+            "bDestroy": true
         });
     };
 
     var displayResults = function(data) {
-        var tables = jquery.fn.dataTable.fnTables(false);
-
-        if (tables.length > 0) {
-            repopulateTable(jquery(tables[0]), data);
-        } else {
-            return createNewTable(data);
-        }
+        return createNewTable(data);
     };
 
-    jquery(":button").click(function() {
-         var getRequest = jquery("#get").is(":checked");
+    jquery("form").submit(function(event) {
+        var getRequest = jquery("#get").is(":checked");
 
-         var type = getRequest ? "GET" : "POST";
-         var url = getRequest ? jquery("#url").val() : "http://localhost:8080/movies";
-         var data = getRequest ? {} : { "dir" : jquery("#url").val() };
-         var contentType = getRequest ? "application/json; charset=UTF-8"
-            : "application/x-www-form-urlencoded; charset=UTF-8";
-         var dataType = "json";
+        var type = getRequest ? "GET" : "POST";
+        var url = getRequest ? jquery(".url").val() : "http://localhost:8080/movies";
+        var data = getRequest ? {} : { "dir" : jquery(".url").val() };
+        var contentType = getRequest ? "application/json; charset=UTF-8"
+        : "application/x-www-form-urlencoded; charset=UTF-8";
+        var dataType = "json";
 
-         var success = function(data, textStatus, jqXHR) {
+        var success = function(data, textStatus, jqXHR) {
             jquery(".searchResults").show();
 
             displayResults(data);
-         };
-         var error = function(jqXHR, textStatus, errorThrown) {
+        };
+        var error = function(jqXHR, textStatus, errorThrown) {
             jquery(".searchResults").hide();
             alert(textStatus);
-         };
+        };
 
-         jquery.ajax({
+        jquery.ajax({
             contentType: contentType,
             data: data,
             dataType: dataType,
@@ -91,6 +79,8 @@ require(["jquery", "dataTables"], function(jquery, dataTables) {
             success: success,
             type: type,
             url: url
-         });
+        });
+
+        return false;
     });
 });
